74cd9d186e2cfc750b0703bd82231361c0350f91
Bruna Antelo <167141335+brunitaa@users.noreply.github.com>
2026-05-21 00:13:46 -0400

    Update vite.config.js

--- ECOO/frontend/landing/vite.config.js
+++ ECOO/frontend/landing/vite.config.js
@@ -34,6 +34,6 @@ export default defineConfig({
   },
   server: {
     port: 5170,
-    proxy: { "/api": { target: "http://localhost:3001", changeOrigin: true } },
+    proxy: { "/api": { target: "https://ecoo-backend.onrender.com", changeOrigin: true } },
   },
 });
